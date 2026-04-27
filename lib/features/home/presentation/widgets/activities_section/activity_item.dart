f560419cde22897e279d7f4c18576cd1d09d7ca8
Karim Motaz <karemmotaz2929@gmail.com>
2026-04-27 18:31:32 +0300

    Edit activities UI part 2

--- lib/features/home/presentation/widgets/activities_section/activity_item.dart
+++ lib/features/home/presentation/widgets/activities_section/activity_item.dart
@@ -1,6 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 import '../../../../../core/theming/app_colors.dart';
 import '../../../../../core/theming/app_styles.dart';
 import '../../../data/models/activity/activity_model.dart';
@@ -19,13 +18,31 @@ class ActivityItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const SizedBox(height: 10),
-          SvgPicture.network(activity.imageUrl, width: 240),
+          CachedNetworkImage(
+            imageUrl: activity.imageUrl,
+            fadeInDuration: const Duration(milliseconds: 200),
+            placeholder: (context, url) => Container(
+              color: Colors.grey.shade300,
+              child: const Center(
+                child: CircularProgressIndicator(strokeWidth: 2),
+              ),
+            ),
+
+            errorWidget: (context, url, error) => Container(
+              color: Colors.grey,
+              child: const Icon(Icons.broken_image),
+            ),
+
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: imageProvider),
+              ),
+            ),
+          ),
           const SizedBox(height: 35),
           Text(
             activity.title,
-            style: AppStyles.extraBold27.copyWith(
-              color: AppColors.brandGreen,
-            ),
+            style: AppStyles.extraBold27.copyWith(color: AppColors.brandGreen),
           ),
           const SizedBox(height: 15),
           Text(
